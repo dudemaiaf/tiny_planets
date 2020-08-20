2d4148456d7af8cdd115a8134dae790f50998aef
dudemaiaf <dudu_maia13@hotmail.com>
2020-08-19 21:52:39 -0400

    smartphone1

---
+++ SmartPhone1/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    bool moveAllowed;
+    Collider2D collider;
+    public GameObject selectionEffect;
+    public GameObject deathEffect;
+    [SerializeField]
+    private AudioSource[] source;
+
+    private GameMaster gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponents<AudioSource>();
+        collider = GetComponent<Collider2D>();
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0 )
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if(touch.phase == TouchPhase.Began)
+            {
+                Collider2D touchedCollider = Physics2D.OverlapPoint(touchPosition);
+                if(collider == touchedCollider)
+                {
+                    Instantiate(selectionEffect, transform.position, Quaternion.identity);
+                    source[0].Play();
+                    moveAllowed = true;
+                }
+            }
+            if(touch.phase == TouchPhase.Moved)
+            {
+                if(moveAllowed)
+                {
+                    transform.position = new Vector2(touchPosition.x, touchPosition.y);
+                }
+            }
+            if(touch.phase == TouchPhase.Ended)
+            {
+                moveAllowed = false;
+            }
+        }
+    }
+
+        private void OnTriggerEnter2D (Collider2D collision)
+    {
+        if(collision.tag == "Atomos")
+        {
+            source[1].Play();
+            Instantiate(deathEffect, transform.position, Quaternion.identity);
+            gm.GameOver();
+        }
+    }
+}
---
+++ SmartPhone1/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour
+{
+    public GameObject restartPanel;
+    private bool asLost;
+    public Text timerDisplay;
+    public float timer;
+    public void GoToGameScene ()
+    {
+        SceneManager.LoadScene("Fase1");
+    }
+
+    public void restart ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void GoToMainMenu ()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GameOver ()
+    {
+        asLost = true;
+        Invoke("Delay", 1.5f);
+    }
+
+    public void Delay ()
+    {
+        restartPanel.SetActive(true);
+    }
+    
+    private void Update ()
+    {
+        if(asLost == false)
+        {
+            timerDisplay.text = timer.ToString("F0");
+        }
+
+        if(timer <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }else{
+            timer -= Time.deltaTime;
+        }
+    }
+
+}
